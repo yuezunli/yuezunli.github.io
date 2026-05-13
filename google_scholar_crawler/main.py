745412d88ff9394123f4d54d6b75e74fd1adc595
Yuezun Li <liyuezun@outlook.com>
2026-05-13 11:56:46 +0800

    update

---
+++ google_scholar_crawler/main.py
@@ -0,0 +1,28 @@
+from scholarly import scholarly, ProxyGenerator
+import jsonpickle
+import json
+from datetime import datetime
+import os
+
+# Setup proxy
+pg = ProxyGenerator()
+pg.FreeProxies()  # Use free rotating proxies
+scholarly.use_proxy(pg)
+
+author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+name = author['name']
+author['updated'] = str(datetime.now())
+author['publications'] = {v['author_pub_id']:v for v in author['publications']}
+print(json.dumps(author, indent=2))
+os.makedirs('results', exist_ok=True)
+with open(f'results/gs_data.json', 'w') as outfile:
+    json.dump(author, outfile, ensure_ascii=False)
+
+shieldio_data = {
+  "schemaVersion": 1,
+  "label": "citations",
+  "message": f"{author['citedby']}",
+}
+with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
+    json.dump(shieldio_data, outfile, ensure_ascii=False)
